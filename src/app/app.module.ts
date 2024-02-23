0d066f4fa1e0b72a2c26c3febf339b61d88a1103
Beukens <beukensgaetan@gmail.com>
2024-02-23 08:16:12 +0100

    Add navigation components and PrimeNG library
    The navigation components including header, navbar, and footer were added while simultaneously implementing visual styles for these components. The PrimeNG and PrimeIcons libraries were also integrated to enhance UI components and provide additional icons for navigation. This commit also includes changes in angular.json and package-lock.json to accommodate these new integrations.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import {MegaMenuModule} from "primeng/megamenu";
+import { FooterComponent } from './shared/footer/footer.component';
+import {ImageModule} from "primeng/image";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavbarComponent,
+    FooterComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        MegaMenuModule,
+        ImageModule,
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/shared/navbar/navbar.component.ts
@@ -0,0 +1,22 @@
+import {Component, OnInit} from '@angular/core';
+import {MegaMenuItem, PrimeIcons} from "primeng/api";
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent implements OnInit{
+  items!: MegaMenuItem[]
+  ngOnInit() {
+    this.items= [
+      {label:"Acceuil"},
+      {label:"Client"},
+      {label:"Chien"},
+      {label:"Rendez-vous"},
+      {icon: PrimeIcons.POWER_OFF}
+    ]
+  }
+
+
+}
